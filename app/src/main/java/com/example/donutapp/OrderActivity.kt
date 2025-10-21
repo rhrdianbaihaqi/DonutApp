a154da3e94dcae7da292e66f76a463de61b66609
rhrdianbaihaqi <haqistream@gmail.com>
2025-10-21 14:53:49 +0700

    Commit Tugas

--- app/src/main/java/com/example/donutapp/OrderActivity.kt
+++ app/src/main/java/com/example/donutapp/OrderActivity.kt
@@ -2,45 +2,59 @@ package com.example.donutapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class OrderActivity : AppCompatActivity() {
+class OrderActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+        val citySpinner: Spinner = findViewById(R.id.city_spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.cities_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            citySpinner.adapter = adapter
+        }
+
+        citySpinner.onItemSelectedListener = this
     }
 
-
     fun onRadioButtonClicked(view: View) {
-        // Is the button now checked?
         val checked = (view as RadioButton).isChecked
         when (view.getId()) {
-            R.id.sameday -> if (checked) // Same day service
+            R.id.sameday -> if (checked)
                 displayToast(getString(R.string.same_day_messenger_service))
 
-
-            R.id.nextday -> if (checked) // Next day delivery
+            R.id.nextday -> if (checked)
                 displayToast(getString(R.string.next_day_ground_delivery))
 
-
-            R.id.pickup -> if (checked) // Pick up
+            R.id.pickup -> if (checked)
                 displayToast(getString(R.string.pick_up))
-
 
             else -> {}
         }
     }
-
 
     private fun displayToast(message: String) {
         Toast.makeText(
             applicationContext, message,
             Toast.LENGTH_SHORT
         ).show()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val selectedCity = parent.getItemAtPosition(pos).toString()
+        displayToast("City selected: $selectedCity")
+    }
+    override fun onNothingSelected(parent: AdapterView<*>) {
     }
 }
